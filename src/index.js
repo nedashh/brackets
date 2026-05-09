fba9530866b9787f03ab858b435a8257aef776a8
Omurzakova Datkaiym <gghtryhrrg@gmail.com>
2026-05-09 15:01:30 +0300

    feat: implement brackets validation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
-module.exports = function check(/* str, bracketsConfig */) {
-  throw new Error('Not implemented');
+module.exports = function check(str, bracketsConfig) {
+  const stack = [];
+  const openBrackets = {};
+  const closeBrackets = {};
+  const sameBrackets = new Set();
+
+  bracketsConfig.forEach((config) => {
+    const [open, close] = config;
+
+    openBrackets[open] = close;
+    closeBrackets[close] = open;
+
+    if (open === close) {
+      sameBrackets.add(open);
+    }
+  });
+
+  for (let i = 0; i < str.length; i += 1) {
+    const char = str[i];
+
+    if (sameBrackets.has(char)) {
+      if (stack[stack.length - 1] === char) {
+        stack.pop();
+      } else {
+        stack.push(char);
+      }
+    } else if (openBrackets[char]) {
+      stack.push(char);
+    } else if (closeBrackets[char]) {
+      const lastOpen = stack.pop();
+
+      if (closeBrackets[char] !== lastOpen) {
+        return false;
+      }
+    }
+  }
+
+  return stack.length === 0;
 };
